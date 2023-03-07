ce6be6be26d258ccd542238c804a3eeca2e836e9
The-Able <angelprodev@gmail.com>
2023-03-07 19:40:44 +0600

    flying api

--- views/src/api/ChatRequest.js
+++ views/src/api/ChatRequest.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const API = axios.create({baseURL: "http://localhost:8080"})
+const API = axios.create({baseURL: "http://social-media-inky.vercel.app"})
 
 
 export const userChats = id => API.get(`/chat/${id}`)
--- views/src/api/MessageRequest.js
+++ views/src/api/MessageRequest.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const API = axios.create({baseURL: "http://localhost:8080"})
+const API = axios.create({baseURL: "http://social-media-inky.vercel.app"})
 
 
 export const getMessages = id => API.get(`/message/${id}`)
